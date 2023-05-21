359014e2d6d1ade9904acae9b5a14616aadbc65b
Aniket <t.aniket561@gmail.com>
2023-05-21 20:46:15 +0530

    complete exercise 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,20 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    if(num1 < 0 || num2 < 0){
+        return "ERROR";
+    }
 
+    if(!Number.isInteger(num1) || !Number.isInteger(num2)){
+        return "ERROR";
+    }
+
+    const a = Math.min(num1, num2);
+    const b = Math.max(num1, num2);
+
+    let sum = 0;
+    for(let i = a; i <= b; i++){
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
